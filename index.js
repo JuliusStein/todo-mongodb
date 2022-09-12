37432ffddb0fbcbfd27bf5efc7bce2184014ce89
JuliusStein <juliusastein@gmail.com>
2022-09-12 00:23:50 -0400

    POST, EDIT, and DELETE working

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ dotenv.config();
 
 app.use("/static", express.static("public"));
 
-app.get('/',(req, res) => {
-    res.render('todo.ejs');
-    });
-
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
@@ -31,9 +27,49 @@ app.post('/',async (req, res) => {
     }
 });
 
-// app.post('/',(req, res) => {
-//     console.log(req.body);
-// });
+
+//GET METHOD
+app.get("/", (req, res) => {
+    try{
+        TodoTask.find({}, (err, tasks) => {
+           //console.log(tasks);
+            if(tasks == null){
+                tasks = [];
+            }
+            res.render("todo.ejs", { todoTasks: tasks });
+        });
+    }catch (err){
+        console.log(err);
+        res.redirect("/");
+    }
+    
+});
+
+//UPDATE
+app
+    .route("/edit/:id")
+    .get((req, res) => {
+        const id = req.params.id;
+        TodoTask.find({}, (err, tasks) => {
+            res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+        });
+    })
+    .post((req, res) => {
+        const id = req.params.id;
+        TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+            if (err) return res.send(500, err);
+            res.redirect("/");
+        });
+    });
+
+//DELETE
+app.route("/remove/:id").get((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndRemove(id, err => {
+        if (err) return res.send(500, err);
+        res.redirect("/");
+    });
+});
 
 //connection to db
 //mongoose.set("useFindAndModify", false);
